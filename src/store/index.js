10b7d83caf16febe996b24740754c13dffd8e268
canyucelbal <c_bal@fanshaweonline.ca>
2021-05-25 18:53:05 +0300

    add modules

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import cart from './modules/cart'
+import products from './modules/products'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    cart,
+    products,
+  },
+  state: {},
+  mutations: {},
+  actions: {},
+})
---
+++ src/store/modules/cart.js
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+
+// STATE
+const state = () => ({})
+
+// MUTATIONS
+const mutations = {}
+
+// ACTIONS
+const actions = {}
+
+// GETTERS
+const getters = {}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+}
