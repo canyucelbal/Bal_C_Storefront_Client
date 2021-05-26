462db107d5763aded8f9fd31c089f32efdb91bed
canyucelbal <c_bal@fanshaweonline.ca>
2021-05-26 19:17:10 +0300

    finish products module

--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -1,16 +1,45 @@
 /* eslint-disable prettier/prettier */
+import axios from 'axios'
 
 // STATE
-const state = () => ({})
+const state = () => ({
+  products: [],
+  product: null,
+})
 
 // MUTATIONS
-const mutations = {}
+const mutations = {
+  products(state, products) {
+    state.products = products
+  },
+  product(state, product) {
+    state.product = product
+  },
+}
 
 // ACTIONS
-const actions = {}
+const actions = {
+  async fetchProducts({ commit }, queryString = '') {
+    const url = `http://localhost:3030/api/products?${queryString}`
+    const result = await axios.get(url)
+    commit('products', result.data.data)
+  },
+  async fetchSingleProduct({ commit }, productId) {
+    const url = `http://localhost:3030/api/products/${productId}`
+    const result = await axios.get(url)
+    commit('product', result.data.data)
+  },
+}
 
 // GETTERS
-const getters = {}
+const getters = {
+  products(state) {
+    return state.products
+  },
+  product(state) {
+    return state.product
+  },
+}
 
 export default {
   namespaced: true,
