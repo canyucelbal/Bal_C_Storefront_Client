e24058cba91b736df2c1991707557ef02e159256
canyucelbal <c_bal@fanshaweonline.ca>
2021-05-28 18:30:37 +0300

    add scss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,42 @@
 <template>
   <div id="app">
-    <router-view />
+    <div class="navbar">
+      <p @click="$router.push('/')" class="brand-logo is-clickable">
+        <i class="fas fa-bicycle" /> Bikestore
+      </p>
+
+      <span class="right">
+        <div @click="$router.push('/cart')" class="cart">
+          <span class="price"> {{ cartPrice }}$ </span>
+          <span class="icon"><i class="fas fa-shopping-cart" /></span>
+        </div>
+      </span>
+    </div>
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
 
-<style></style>
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  created() {
+    this.loadCart();
+  },
+  computed: {
+    ...mapGetters({
+      cartPrice: "cart/cartPrice",
+    }),
+  },
+  methods: {
+    ...mapActions({
+      loadCart: "cart/load",
+    }),
+  },
+};
+</script>
+
+<style lang="sass">
+@import "./assets/main.scss"
+</style>
